d134299fb8cd5c8563a6b5b89f8f3beda7285244
banaag <banaag@google.com>
2021-11-19 09:40:37 -0800

    updates from PR comments set 1.

--- dialog.js
+++ dialog.js
@@ -118,7 +118,8 @@ function setDisplayFields(result, urlFieldId, contentTypeFieldId, imgFieldId) {
 const setDOMInfo = info => {
     fetch(info.url, {
       method: "GET",
-      headers: { "Accept": "application/signed-exchange;v=b3"}
+      headers: {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"}
     }).then(result => {
       setDisplayFields(result, 'url', 'contenttype', 'originimg');
     })
