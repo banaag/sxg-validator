1af6119574ecb4de13c2d68fca4c4290d3396b07
Allan Banaag <banaag@google.com>
2021-12-10 14:08:33 -0800

    Initial commit of SXG Validator Chrome Extension. (#1)
    * Initial commit of SXG Validator Chrome Extension.
    
    * updates from PR comments set 1.
    
    * updates from PR comments set 2.
    
    * updates from PR comments set 3.
    
    * Updates from PR comments set 4.
    
    * Updated readme with build instructions.
    
    * Update accept header for fetching SXG to use googlebot value.
    
    * Address latest PR comments
    
    * Address latest PR comments
    
    * Address latest PR comments

---
+++ build_extension.sh
@@ -0,0 +1,5 @@
+npm init -y
+npm install google-closure-library
+npm install --save-dev google-closure-compiler
+npm run build
+
---
+++ package_extension.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+#
+# A script to package only the necessary files of the extension.
+
+echo "Building chrome extension"
+
+./build_extension.sh
+
+echo "Packaging chrome extension"
+
+VERSION=$(egrep "\"version\":" manifest.json | cut -d\" -f4)
+zip -r extension-"$VERSION".zip ./ -x package_extension.sh README.md node_modules/**\* third_party/**\*
+
+echo "Done"
